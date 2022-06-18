4ceb1ba580d8f5ae295c68ceb72cc092a96707ab
Christopher C <cristophercalderon98@gmail.com>
2022-06-17 22:42:14 -0600

    Se agrega: Crear usuarios. v1.1

---
+++ Dream_Flights/Controllers/SignInController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.Json;
+using Dream_Flights.Models;
+
+namespace Dream_Flights.Controllers
+{
+    public class SignInController : Controller
+    {
+        // GET: SignInController
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SignInUser()
+        {
+            return View();
+        }
+
+        public CreateUserModel CreateUser(string name, string first_name, string last_name, string password, string email)
+        {
+            List<SqlParameter> param = new List<SqlParameter>();
+
+            param.Add(new SqlParameter("@per_name", name));
+            param.Add(new SqlParameter("@per_first_name", first_name));
+            param.Add(new SqlParameter("@per_last_name", last_name));
+            param.Add(new SqlParameter("@per_email", email));
+            param.Add(new SqlParameter("@per_img", "default.jpg"));
+            param.Add(new SqlParameter("@user_password", password));
+            param.Add(new SqlParameter("@id_rol", 5));
+
+
+            DatabaseHelper.DatabaseHelper.ExecStoreProcedure("sp_insert_user", param);
+
+            return null;
+        }
+
+        // GET: SignInController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+
+        }
+
+        // GET: SignInController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SignInController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SignInController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SignInController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SignInController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SignInController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Dream_Flights/Models/CreateUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Dream_Flights.Models
+{
+    public class CreateUserModel
+    {
+            public string per_name { get; set; }
+            public string per_first_name { get; set; }
+            public string per_last_name { get; set; }
+            public string per_email { get; set; }
+            public string per_password { get; set; }
+    }
+}
